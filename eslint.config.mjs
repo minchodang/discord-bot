b5bd68972b4024049b2962a06caf743574640271
minchodang <minsu910725@gmail.com>
2024-06-23 11:59:45 +0900

    Fix: eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,40 +1,43 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import prettier from "eslint-plugin-prettier";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import prettier from 'eslint-plugin-prettier';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default [...compat.extends(
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-), {
+export default [
+  ...compat.extends(
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ),
+  {
     plugins: {
-        "@typescript-eslint": typescriptEslint,
-        prettier,
+      '@typescript-eslint': typescriptEslint,
+      prettier,
     },
 
     languageOptions: {
-        parser: tsParser,
-        parserOptions: {
-            project: path.resolve(__dirname, './tsconfig.json'),
-            tsconfigRootDir: __dirname,
-        },
+      parser: tsParser,
+      parserOptions: {
+        project: path.resolve(__dirname, './tsconfig.json'),
+        tsconfigRootDir: __dirname,
+      },
     },
 
     rules: {
-        "deprecation/detect": "off",
-        "prettier/prettier": "error",
-        "@typescript-eslint/strict-boolean-expressions": "warn",
+      'deprecation/detect': 'off',
+      'prettier/prettier': 'error',
+      '@typescript-eslint/strict-boolean-expressions': 'warn',
     },
-}];
+  },
+];
