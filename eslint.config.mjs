1b981acd1ae1a3851fbaae0b82c591a886785b8c
minsuKang <90169703+minchodang@users.noreply.github.com>
2024-06-24 22:04:40 +0900

    Feat: Bot 로그인, eslint  (#3)
    * Feat: bot로그인 처리
    
    * Fix: eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,43 @@
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import prettier from 'eslint-plugin-prettier';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  ...compat.extends(
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ),
+  {
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+      prettier,
+    },
+
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: path.resolve(__dirname, './tsconfig.json'),
+        tsconfigRootDir: __dirname,
+      },
+    },
+
+    rules: {
+      'deprecation/detect': 'off',
+      'prettier/prettier': 'error',
+      '@typescript-eslint/strict-boolean-expressions': 'warn',
+    },
+  },
+];
