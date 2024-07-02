64f5412305e50963f26a8a680bcde4ac365e067e
minchodang <minsu910725@gmail.com>
2024-07-02 13:58:53 +0900

    Feat: 손들기 기능

--- apps/basic-bot/src/commands/index.ts
+++ apps/basic-bot/src/commands/index.ts
@@ -1,7 +1,8 @@
 import { echo } from './echo';
+import { handsUp } from './handsup';
 import { joke } from './joke';
 import { search } from './search';
 
-const availableCommands = [echo, search, joke];
+const availableCommands = [echo, search, joke, handsUp];
 
 export default availableCommands;
