2e48d2859dbf96bfae823886c20fc4323167605b
minchodang <minsu910725@gmail.com>
2024-07-08 12:13:07 +0900

    Feat: new vote version

--- apps/basic-bot/src/commands/index.ts
+++ apps/basic-bot/src/commands/index.ts
@@ -1,9 +1,10 @@
 import { echo } from './echo';
 import { handsUp } from './handsup';
 import { joke } from './joke';
+import { poll2 } from './pollTest';
 import { search } from './search';
 import { createPoll } from './vote';
 
-const availableCommands = [echo, search, joke, handsUp, createPoll];
+const availableCommands = [echo, search, joke, handsUp, createPoll, poll2];
 
 export default availableCommands;
