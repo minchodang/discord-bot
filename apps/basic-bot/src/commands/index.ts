0fa92de1cc0776984442f1627fcf8fae22827b73
minsuKang <90169703+minchodang@users.noreply.github.com>
2024-07-18 10:21:15 +0900

    feat: 기본 공지 사항 기능 (#25)

--- apps/basic-bot/src/commands/index.ts
+++ apps/basic-bot/src/commands/index.ts
@@ -1,10 +1,19 @@
 import { echo } from './echo';
 import { handsUp } from './handsup';
 import { joke } from './joke';
+import { notice } from './notice';
 import { poll2 } from './pollTest';
 import { search } from './search';
 import { createPoll } from './vote';
 
-const availableCommands = [echo, search, joke, handsUp, createPoll, poll2];
+const availableCommands = [
+  echo,
+  search,
+  joke,
+  handsUp,
+  createPoll,
+  poll2,
+  notice,
+];
 
 export default availableCommands;
